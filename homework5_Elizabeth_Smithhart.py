890d44eeef2bdc0d98ea0386a176961e8db49343
Zabeth1021 <zabeth21@uw.edu>
2025-02-10 05:50:49 +0000

    commit

---
+++ homework5_Elizabeth_Smithhart.py
@@ -0,0 +1,16 @@
+Hockeygear = ['helmet', 'gloves', 'skates', 'stick', 'pads', 'socks']
+print (Hockeygear)
+print ("The first two items in the list are:",Hockeygear[0:2])
+print ("The middle two items in the list are:",Hockeygear[2:4])
+print ("The first and the last items in the list are:",(Hockeygear[0]),"and",(Hockeygear[5])) 
+
+
+RestaurantsIMiss = ('Chuys', 'Whataburger', 'Glorias', 'Pappasitos', 'El Chapparral')
+for menu in RestaurantsIMiss:
+    print(menu)
+
+
+RestaurantsIMiss = ('Chuys', 'Whataburger', 'Freebirds', 'Cafe LiLi', 'El Chapparral')
+print("My list has changed")
+for menu in RestaurantsIMiss:
+    print(menu)
